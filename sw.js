6ebc8c286dab9955263e20162806acbdd303f5d9
joaubaron <joaubaron@users.noreply.github.com>
2026-04-29 15:17:47 +0000

    deploy: bbc41fe91f8f57dbccd853f602ec504553266e73

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '29.04.2026-1214';
+const CACHE_VERSION = '29.04.2026-1217';
 const CACHE_NAME = `brasileirao-${CACHE_VERSION}`;
 const ASSETS = [
   '/brasileirao/manifest.json',
