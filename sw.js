86cd5891d22ed9efff7ca3c53688b69d755284b0
joaubaron <joaubaron@users.noreply.github.com>
2026-04-30 12:34:19 +0000

    deploy: e9b401bcff4c904f40d7b9586ebc6c538ff4468d

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '30.04.2026-0921';
+const CACHE_VERSION = '30.04.2026-0934';
 const CACHE_NAME = `brasileirao-${CACHE_VERSION}`;
 const ASSETS = [
 '/brasileirao/icon-192.png',
